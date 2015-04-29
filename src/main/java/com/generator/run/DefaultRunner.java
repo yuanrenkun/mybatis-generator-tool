41ee6e85d05f33b6e4013f052ef3403de7765082
tanxianwen <tanxianwen@36kr.com>
2015-04-29 11:31:56 +0800

    refactor

--- src/main/java/com/generator/run/DefaultRunner.java
+++ src/main/java/com/generator/run/DefaultRunner.java
@@ -2,6 +2,11 @@ package com.generator.run;
 
 import org.mybatis.generator.api.ShellRunner;
 
+/**
+ * 运行默认的配置文件
+ * @author tanxianwen 
+ * 2015年4月29日
+ */
 public class DefaultRunner {
     public static void main(String[] args) {
         String config = DefaultRunner.class.getClassLoader().getResource("generatorConfig.xml").getFile();
--- src/main/java/com/generator/run/XiangQianRunner.java
+++ src/main/java/com/generator/run/XiangQianRunner.java
@@ -2,6 +2,11 @@ package com.generator.run;
 
 import org.mybatis.generator.api.ShellRunner;
 
+/**
+ * 运行幸倩的配置文件
+ * @author tanxianwen 
+ * 2015年4月29日
+ */
 public class XiangQianRunner {
     public static void main(String[] args) {
         String config = XiangQianRunner.class.getClassLoader().getResource("xingqian_generatorConfig.xml").getFile();
