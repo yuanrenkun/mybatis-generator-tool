b54a79d3fe9f054e68881db329bdf7dd7d87a8b3
haokuixi <haokuixi@36kr.com>
2015-05-06 10:25:10 +0800

    update

--- src/main/java/com/generator/run/HaokuixiRunner.java
+++ src/main/java/com/generator/run/HaokuixiRunner.java
@@ -1,14 +1,18 @@
 package com.generator.run;
 
+import org.apache.ibatis.io.Resources;
 import org.mybatis.generator.api.ShellRunner;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author haokuixi
  */
 public class HaokuixiRunner {
-    public static void main(String[] args) {
-        String config = HaokuixiRunner.class.getClassLoader().getResource("generatorConfig.xml").getFile();
-        String[] arg = {"-configfile", config, "-overwrite", "-verbose", "-tables", "xxx"};
+    public static void main(String[] args) throws IOException {
+        File configFile = Resources.getResourceAsFile("generatorConfig.xml");
+        String[] arg = {"-configfile", configFile.getPath(), "-overwrite", "-verbose", "-tables", "xxx"};
         ShellRunner.main(arg);
     }
 }
