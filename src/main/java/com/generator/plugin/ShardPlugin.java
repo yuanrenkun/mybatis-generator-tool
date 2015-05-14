f04faaca53943ec54ddb72d99113087f564e1c11
micuncang <micuncang@36kr.com>
2015-05-14 15:12:20 +0800

    添加散表插件

---
+++ src/main/java/com/generator/plugin/ShardPlugin.java
@@ -0,0 +1,33 @@
+package com.generator.plugin;
+
+import java.util.List;
+
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.config.TableConfiguration;
+
+/**
+ * 类上添加散表字段
+ * 
+ * @author Chris Mee
+ * @Email micuncang@36kr.com 2015年5月14日
+ */
+public class ShardPlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        TableConfiguration tc = introspectedTable.getTableConfiguration();
+        String shardKey = tc.getProperty("shardBy");
+        StringBuffer sb = new StringBuffer();
+        sb.append("@ShardBy(\"").append(shardKey).append("\")");
+        topLevelClass.addAnnotation(sb.toString());
+        // @ShardBy注解可以作为基本类型并入common-util, 此处不进行引入强制
+        topLevelClass.addImportedType("");
+        return true;
+    }
+}
---
+++ src/main/java/com/generator/run/ChrisMeeRunner.java
@@ -0,0 +1,16 @@
+package com.generator.run;
+
+import org.mybatis.generator.api.ShellRunner;
+
+/**
+ * 运行默认的配置文件
+ * @author tanxianwen 
+ * 2015年4月29日
+ */
+public class ChrisMeeRunner {
+    public static void main(String[] args) {
+        String config = ChrisMeeRunner.class.getClassLoader().getResource("ChrisMeeGeneratorConfig.xml").getFile();
+        String[] arg = { "-configfile", config, "-overwrite", "-verbose", "-tables", "user_mini_feed" };
+        ShellRunner.main(arg);
+    }
+}
