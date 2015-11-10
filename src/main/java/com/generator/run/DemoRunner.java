63981b3df85e3a34fddbf1519c82d1dd1639bf2e
micuncang <micuncang@36kr.com>
2015-11-11 00:56:14 +0800

    add demo

---
+++ src/main/java/com/generator/run/DemoRunner.java
@@ -0,0 +1,29 @@
+package com.generator.run;
+
+import org.mybatis.generator.api.ConfigurationParserCustomizedShellRunner;
+import org.mybatis.generator.config.xml.KrConfigurationParser;
+
+/**
+ * @author Chris Mee
+ * @Email micuncang@36kr.com 2015年11月11日
+ */
+public class DemoRunner {
+    public static void main(String[] args) {
+        String config = DemoRunner.class.getClassLoader().getResource("Demo.xml").getFile();
+        String[] arg = {"-configfile", config, "-overwrite", "-verbose", "-contextids", "krplus2", "-tables", "assistor_msg"};
+
+        ConfigurationParserCustomizedShellRunner.configuration_parser_clazz = KrConfigurationParser.class;
+
+        KrConfigurationParser.jdbcConnection_connectionURL = "jdbc:mysql://127.0.0.1:33061/krplus2";
+        KrConfigurationParser.jdbcConnection_driverClass = "com.mysql.jdbc.Driver";
+        KrConfigurationParser.jdbcConnection_password = "MUe0glR9XjnYfLn5";
+        KrConfigurationParser.jdbcConnection_userId = "krplus";
+
+        KrConfigurationParser.initTargetProject("/Users/mcc/Downloads/mysqlgenerator2");
+        KrConfigurationParser.javaClientGenerator_targetPackage = "com.kr.rong.mapper.app.v3.assistor";
+        KrConfigurationParser.javaModelGenerator_targetPackage = "com.kr.rong.model.app.v3.assistor";
+        KrConfigurationParser.sqlMapGenerator_targetPackage = "com.kr.rong.mapper.app.v3.assistor";
+
+        ConfigurationParserCustomizedShellRunner.main(arg);
+    }
+}
