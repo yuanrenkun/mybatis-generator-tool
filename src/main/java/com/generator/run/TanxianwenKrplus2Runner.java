924cb4a0fd37fb91c8a99c132c844954a835dca8
tanxianwen <tanxianwen@36kr.com>
2015-12-15 16:12:59 +0800

    dev

--- src/main/java/com/generator/run/TanxianwenKrplus2Runner.java
+++ src/main/java/com/generator/run/TanxianwenKrplus2Runner.java
@@ -15,17 +15,19 @@ public class TanxianwenKrplus2Runner {
         r.setConfigFile(TanxianwenKrplus2Runner.class.getClassLoader().getResource("krplus2_generatorConfig.xml").getFile());
         r.setContextIds("context1");
         r.setTargetProject("D:/work/gitlab/krplus-service/src/main/java");
-        //r.setJavaModelTargetPackage("com.kr.rong.model.company.test");
-        r.setJavaModelTargetPackage("com.kr.rong.model.blacklist");
-        //String mapperPackage = "com.kr.rong.mapper.company.test";
-        String mapperPackage = "com.kr.rong.mapper.blacklist";
+        // r.setJavaModelTargetPackage("com.kr.rong.model.company.test");
+        r.setJavaModelTargetPackage("com.kr.rong.model.activity.letter");
+        // String mapperPackage = "com.kr.rong.mapper.company.test";
+        // String mapperPackage = "com.kr.rong.mapper.blacklist";
+        String mapperPackage = "com.kr.rong.mapper.activity.letter";
         r.setJavaClientTargetPackage(mapperPackage);
         r.setSqlMapTargetPackage(mapperPackage);
 
         // 设置要生成的表
-        //r.addFullyQualifiedTable("company");
-        //r.addFullyQualifiedTable("company_funds");
-        r.addFullyQualifiedTable("user_blacklist");
+        // r.addFullyQualifiedTable("company");
+        // r.addFullyQualifiedTable("company_funds");
+        // r.addFullyQualifiedTable("user_blacklist");
+        r.addFullyQualifiedTable("activity_company_letter");
 
 
         r.run();
