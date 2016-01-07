9f1d5a20bd03ea06c2209ff0ada0af0410e9410e
quxiao <quxiao@36kr.com>
2016-01-07 10:46:34 +0800

    添加user表的忽略

--- src/main/java/com/generator/run/QuXiaoRunner.java
+++ src/main/java/com/generator/run/QuXiaoRunner.java
@@ -10,17 +10,16 @@ public class QuXiaoRunner {
     public static void main(String[] args) {
         Runner r = new Runner();
 
-        r.setConfigFile(TanxianwenKrplus2Runner.class.getClassLoader().getResource("krplus2_generatorConfig.xml").getFile());
+        r.setConfigFile(QuXiaoRunner.class.getClassLoader().getResource("krplus2_generatorConfig.xml").getFile());
         r.setContextIds("context1");
         r.setTargetProject("E:/code");
-        r.setJavaModelTargetPackage("com.kr.rong.model.company");
-        String mapperPackage = "com.kr.rong.mapper.company";
+        r.setJavaModelTargetPackage("com.kr.rong.model.user");
+        String mapperPackage = "com.kr.rong.mapper.user";
         r.setJavaClientTargetPackage(mapperPackage);
         r.setSqlMapTargetPackage(mapperPackage);
 
         // 设置要生成的表
-        r.addFullyQualifiedTable("company_funds");
-        r.addFullyQualifiedTable("company");
+        r.addFullyQualifiedTable("user");
 
 
         r.run();
