11e066dda7e9541c0c9a0a96e38f6f641058c384
zhangwei <zhangwei@36kr.com>
2016-01-12 12:48:32 +0800

    save_companyType

--- src/main/java/com/generator/run/ZhangWeiRunner.java
+++ src/main/java/com/generator/run/ZhangWeiRunner.java
@@ -1,16 +1,24 @@
 package com.generator.run;
 
-import org.mybatis.generator.api.ShellRunner;
-
 /**
  * 运行前的配置文件
- * @author zhangwei
- * 2015年11月9日
+ *
+ * @author zhangwei 2015年11月9日
  */
 public class ZhangWeiRunner {
     public static void main(String[] args) {
-        String config = ZhangWeiRunner.class.getClassLoader().getResource("zhangwei_generatorConfig.xml").getFile();
-        String[] arg = { "-configfile", config, "-overwrite" , "-verbose", "-tables", "krlass_tutor" };
-        ShellRunner.main(arg);
+        Runner runner = new Runner();
+        runner.setConfigFile(ZhangWeiRunner.class.getClassLoader().getResource("krplus2_generatorConfig.xml").getFile());
+        runner.setContextIds("context1");
+        runner.setTargetProject("/Users/36kr/krspace/krplus-service/src/main/java");
+        runner.setJavaModelTargetPackage("com.kr.rong.model.company");
+        String mapperPackage = "com.kr.rong.mapper.company";
+        runner.setSqlMapTargetPackage(mapperPackage);
+        runner.setJavaClientTargetPackage(mapperPackage);
+
+        //设置生成的表
+//        runner.addFullyQualifiedTable("company");
+
+        runner.run();
     }
 }
