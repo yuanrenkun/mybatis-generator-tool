83a16338747fd5f022652afb6881d280a17adf41
quxiao <quxiao@36kr.com>
2016-03-17 14:11:04 +0800

    添加了创业服务的两张表

--- src/main/java/com/generator/run/QuXiaoRunner.java
+++ src/main/java/com/generator/run/QuXiaoRunner.java
@@ -19,8 +19,8 @@ public class QuXiaoRunner {
         r.setSqlMapTargetPackage(mapperPackage);
 
         // 设置要生成的表
-        r.addFullyQualifiedTable("service_display_order");
-        r.addFullyQualifiedTable("service_customer");
+        r.addFullyQualifiedTable("service_act_best_provider");
+        r.addFullyQualifiedTable("service_act_best_provider_support_record");
 
 
         r.run();
