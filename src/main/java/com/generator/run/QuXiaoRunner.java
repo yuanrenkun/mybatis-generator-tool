12f9c4ecf76b4334a9ffbea8bb1fffd12846fbb5
quxiao <quxiao@36kr.com>
2016-04-13 15:10:05 +0800

    修改表

--- src/main/java/com/generator/run/QuXiaoRunner.java
+++ src/main/java/com/generator/run/QuXiaoRunner.java
@@ -19,7 +19,7 @@ public class QuXiaoRunner {
         r.setSqlMapTargetPackage(mapperPackage);
 
         // 设置要生成的表
-        r.addFullyQualifiedTable("service_tag");
+        r.addFullyQualifiedTable("service_customer");
 
         r.run();
     }
