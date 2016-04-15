e7bedcc95870f59c47471ba8b9cd5c2b091289d7
quxiao <quxiao@36kr.com>
2016-04-15 18:32:56 +0800

    update

--- src/main/java/com/generator/run/QuXiaoRunner.java
+++ src/main/java/com/generator/run/QuXiaoRunner.java
@@ -19,8 +19,10 @@ public class QuXiaoRunner {
         r.setSqlMapTargetPackage(mapperPackage);
 
         // 设置要生成的表
+//        r.addFullyQualifiedTable("service_customer");
         r.addFullyQualifiedTable("service_invite_comment");
 //        r.addFullyQualifiedTable("service_comment_invite_record");
+//        r.addFullyQualifiedTable("service_latest_invite_comment");
 
         r.run();
     }
